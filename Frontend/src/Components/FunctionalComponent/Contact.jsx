e963f16f821f85f31e7d396c1acc9509c50c7445
mr_black_manjuvirattu_lover <karuppusamysivanandam@gmail.com>
2025-02-19 14:46:26 +0530

    useEffect

--- Frontend/src/Components/FunctionalComponent/Contact.jsx
+++ Frontend/src/Components/FunctionalComponent/Contact.jsx
@@ -1,12 +1,19 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 
 const Contact = () => {
-    const[text,setText]=useState("");
+    const[text,setText]=useState("hi");
+    const[text1,setText1]=useState("hello");
+    useEffect(()=>{
+        console.log(text)
+        console.log(text1)
+    },[])
   return (
     <div>
-        <p>{text}</p>
-        <input type="text" value={text} onChange={(e)=>setText(e.target.value)} />
-        
+        Type Your Text : <input type="text" value={text} onChange={(e)=>setText(e.target.value)} />
+        <h4>the Text is {text}</h4>
+        <h3>This is UseEffect Example</h3>
+        Type Your Text1 : <input type="text" value={text1} onChange={(e)=>setText1(e.target.value)} />
+        <h4>the Text is {text1}</h4>
     </div>
   )
 }
