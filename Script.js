f6895ca78367b6de8d88392659cecb57a8b96f1d
mr_black_manjuvirattu_lover <karuppusamysivanandam@gmail.com>
2025-02-19 08:54:27 +0530

    task

--- Script.js
+++ Script.js
@@ -1,67 +1,20 @@
-// //const
-// const ar=2; //not changeable like let or var
-// let b;
-// var c=true;
-// //array
-// a=[1,6,"abc",true]
-// console.log(typeof(a),a[3])
-// // objects
-// object1={
-//     "firstname":"St.Joseph's",
-//     "lastname":"Institute Of Technology",
-//     "depts":["cse","it","ece","ads"],
-//     "training":"mern"
-// }
-// console.log(object1["depts"])
-// console.log(5||10)
-// console.log(5&&10)
-// console.log(5=='5')
-// //shift - left operator
-// console.log(55<<3) 
-// //right -shift operator
-// console.log(55>>4)
-//bitwise operators
-// a=20
-// b=35
-// console.log(a&b)
-// console.log(a^b)
-// c=1
-// console.log(~c)
-//conditional statement
-// 1.if
-// 2.if ... else
-// 3. nested if
-// 4. else if ladder
-// 5. switch
-// Looping Statements
-/*
-1. while 
-2. do ... while
-3. for(;;)
-*/
-array=[10,20,30,"abc",true,"hello",[10,20,30]]
-object1={
-    "firstname":"St.Joseph's",
-    "lastname":"Institute Of Technology",
-    "depts":["cse","it","ece","ads"],
-    "training":"mern"
+// 1
+function test(){
+    console.log("this is test function")
 }
-// for(i in array){
-//     console.log(i);
-// }
-// for(i in object1){
-//     console.log(i);
-// }
-// for(i of array){
-//     console.log(i)
-//}
-// Object. entries/keys/values
-for({key,value} of Object.entries(object1)){
-    console.log(key,value);
+test()
+// 2
+function test2(name){
+    console.log("My name is "+name+" .Nice to meet you")
 }
-// for(key of object.keys(object1)){
-//     console.log(key,value);
-// }
-Object.entries(object1).forEach((ele)=>{
-    console.log(key,ele);
-})
+test2("Black")
+// 3
+function test3(){
+    return "there is no parameter return"
+}
+console.log(test3())
+// 4
+function test4(a,b){
+    return a*b
+}
+console.log(test4(3,5))
